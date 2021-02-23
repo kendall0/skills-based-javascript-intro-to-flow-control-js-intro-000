413096de6600f06fb4fea09b8374e3662592c23e
Kendall George <kendallanngeorge@gmail.com>
2021-02-23 21:15:46 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,12 +4,6 @@ function basicTeenager(age) {
   }
 }
 
-function teenager(age) {
-  if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
-  } else {
-    retun "You are not a teenager!"
-  }
 
 function ageChecker(age) {
 
